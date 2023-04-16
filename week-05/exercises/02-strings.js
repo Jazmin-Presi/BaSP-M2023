ce0e08bfecd6c16565c26c61178f0894cddf85f8
Jazmin Presidente <jazmin.presi@gmail.com>
2023-04-16 01:41:21 -0300

    BaSP-W05-2023: Finished excercise 2 and started excercise 3

--- week-05/exercises/02-strings.js
+++ week-05/exercises/02-strings.js
@@ -9,17 +9,37 @@ console.log(mary.toUpperCase());
 /*b. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros
 5 caracteres guardando el resultado en una nueva variable (utilizar substring)*/
 
+console.log(mary.substring(0,5));
+
+
 /*c. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los
 últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring)*/
+
+
+console.log(mary.substring(mary.length-3,mary.length));
 
 /*d. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera
 letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable
 (utilizar substring, toUpperCase, toLowerCase y el operador +)*/
 
+console.log(mary.substring(0,1).toUpperCase()+mary.substring(1,mary.length).toLowerCase());
+
 /*e. Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco.
 Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf)*/
+
+var marySpace = 'supercalifragilistico espialidoso';
+var marySpaceBlanc = marySpace.indexOf(' ');
+console.log(marySpaceBlanc);
 
 /*f. Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio)
 Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga
 la primera letra de ambas palabras en mayúscula y las demás letras en minúscula.
 (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +)*/
+
+console.log(marySpace.substring(0,1).toUpperCase()+
+marySpace.substring(1,marySpaceBlanc).toLowerCase()+
+marySpace.substring(marySpaceBlanc+1,marySpaceBlanc+2).toUpperCase()+
+marySpace.substring(marySpaceBlanc+2,marySpace.length).toLowerCase());
+
+
+
--- week-05/exercises/03-arrays.js
+++ week-05/exercises/03-arrays.js
@@ -1,17 +1,42 @@
 /*Arrays*/
 
 /*a. Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
-"Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log)*/
+"Octubre", "Noviembre", "Diciembre"] mostrar por consola los months 5 y 11 (utilizar console.log)*/
 
-/*b. Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort)*/
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+"Octubre", "Noviembre", "Diciembre"];
+
+console.log(months[5],months[11]);
+
+/*b. Ordenar el array de months alfabéticamente y mostrarlo por consola (utilizar sort)*/
+
+var monthsAbc = months.sort();
+
+console.log(monthsAbc);
 
 /*c. Agregar un elemento al principio y al final del array (utilizar unshift y push)*/
 
+var colors = ["red", "blue", "yellow", "green"];
+colors.unshift("purple");
+colors.push("black");
+
+console.log(colors);
+
 /*d. Quitar un elemento del principio y del final del array (utilizar shift y pop)*/
 
+colors.shift();
+colors.pop();
+
+console.log(colors);
+
 /*e. Invertir el orden del array (utilizar reverse)*/
+
+console.log(colors.reverse());
 
 /*f. Unir todos los elementos del array en un único string donde cada mes este
 separado por un guión - (utilizar join)*/
 
-/*g. Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice)*/
+console.log(months.join('-'));
+
+/*g. Crear una copia del array de months que contenga desde Mayo hasta Noviembre (utilizar slice)*/
+
